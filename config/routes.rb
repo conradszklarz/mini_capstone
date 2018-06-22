a1d45cccb4427b43b989b87ecefd0a7ff63db011
conradskittles <conradszklarz@gmail.com>
2018-06-21 21:30:19 -0500

    updated

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
     post "/users" => "users#create"
-    post 'user_token' => 'user_token#create'
+    post '/user_token' => 'user_token#create'
     
   namespace :api do
     get '/products' => 'products#index'
